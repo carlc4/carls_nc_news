9014236da6bb6122da1eb505311c4258b89588b9
Carl Crozier <crozier83@googlemail.com>
2022-02-16 08:06:21 +0000

    Added article by id functionality, testing and error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -40,13 +40,52 @@ describe("GET /api/topics", () => {
       });
   });
 });
+describe("GET /api/articles/:article_id", () => {
+  test("Status: 200", () => {
+    return request(app).get("/api/articles/1").expect(200);
+  });
+  test("Status: 200, returns an object with corresponding keys", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles).toEqual(
+          expect.objectContaining({
+            article_id: expect.any(Number),
+            title: expect.any(String),
+            topic: expect.any(String),
+            author: expect.any(String),
+            body: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+          })
+        );
+      });
+  });
+});
 describe("Error handling", () => {
   test("Status: 404, returns url not found message", () => {
     return request(app)
       .get("/api/NOT-A-VALID-URL")
       .expect(404)
-      .then((response) => {
-        expect(response.body.msg).toEqual("Invalid URL Passed");
+      .then(({ body }) => {
+        expect(body.msg).toEqual("Invalid URL Passed");
+      });
+  });
+  test("Status: 400, not found if article_id is invalid", () => {
+    return request(app)
+      .get("/api/articles/ERROR")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.message).toEqual("Article ID Invalid!");
+      });
+  });
+  test("Status: 404, not found if article_id is valid but no records exist", () => {
+    return request(app)
+      .get("/api/articles/9999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.message).toEqual("Article ID Does Not Exist!");
       });
   });
 });
