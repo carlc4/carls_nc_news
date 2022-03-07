7b96cdada1dc4c8c59591d4d77dc3d64cb581854
Carl Crozier <crozier83@googlemail.com>
2022-03-07 19:11:25 +0000

    Added CORS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
 
 const {
@@ -10,6 +11,8 @@ const {
 } = require("./errors/app.errors");
 
 const apiRouter = require("./routers/apiRouter");
+
+app.use(cors());
 
 app.use("/api", apiRouter);
 
